e37b0c61386ababc60f1f72ed1f82da23c57e3e5
Pantheleymon <pantheleymonbbx@yandex.ru>
2024-01-12 14:42:20 +0300

    Готовый вариант

--- script.js
+++ script.js
@@ -11,48 +11,33 @@ const modalAddTaskInput = document.querySelector('#modal__add_task_input');
 const taskContainer = document.querySelector('#task-container');
 
 
-function taskDivGenerator() {
-    taskContainer.innerHTML = '';
-    let counter = 1;
-    for(let i=0; i<=localStorage.length; i++) {
-        if (localStorage.getItem(`task${i}`) !== null) {
-            let taskDiv = document.createElement('div');
-            taskContainer.appendChild(taskDiv);
-            taskDiv.classList.add('task');
-            taskDiv.innerHTML = `<div class="task-condition">
-                                    <span class="material-symbols-outlined">
-                                        radio_button_unchecked
-                                        </span>
-                                </div>
-                                <div class="task-text">
-                                    <p>${counter}. ${localStorage.getItem(`task${i}`)}</p>
-                                </div>
-                                <div class="task-favorite">
-                                    <span class="material-symbols-outlined">
-                                        star
-                                        </span>
-                                </div>
-
-                                <div class="task-delete">
-                                    <span class="material-symbols-outlined">
-                                        delete
-                                        </span>
-                                </div>`;
-            ++counter;
-        }
-    }
-    let deleteBtnArr = document.querySelectorAll('.task-delete');
-    deleteBtnArr.forEach((el)=>{
-        el.addEventListener('click', ()=>{
-            let index = [...deleteBtnArr].indexOf(el);
-            localStorage.removeItem(`task${index}`);
-            taskDivGenerator();
-        });
-    })
-}
-
 function styleDisplayToggler(item, style) {
     item.style.display = `${style}`;
+}
+
+function saveData() {
+    localStorage.setItem('data', taskContainer.innerHTML);
+}
+
+function showData() {
+    let data = localStorage.getItem('data');
+    taskContainer.innerHTML = data;
+}
+
+function addTask() {
+    let taskText = modalAddTaskInput.value;
+    if (taskText !== null && taskText !== undefined && taskText.length !== 0) {
+        let li = document.createElement('li');
+        li.innerHTML = taskText;
+        taskContainer.appendChild(li);
+
+        let span = document.createElement('span');
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+        saveData();
+    } else {
+        alert("Напишите что-нибудь");
+    }
 }
 
 addTaskBtn.addEventListener('click', ()=>{
@@ -74,14 +59,9 @@ modalAddTaskInput.addEventListener('keypress', (event)=>{
 
 modalAddTaskBtn.addEventListener('click', (el)=>{
     el.preventDefault();
-    let taskText = modalAddTaskInput.value;
-    if (taskText !== null || taskText !== undefined || taskText.length !== 0) {
-        let taskCounter = localStorage.length;
-        localStorage.setItem(`task${taskCounter}`, taskText);
-        taskDivGenerator();
-        modalAddTaskInput.value = '';
-        styleDisplayToggler(modalWindow, 'none');
-    }
+    addTask();
+    modalAddTaskInput.value = '';
+    styleDisplayToggler(modalWindow, 'none');
 });
 
 clearAllTaskBtn.addEventListener('click', (el)=>{
@@ -89,8 +69,19 @@ clearAllTaskBtn.addEventListener('click', (el)=>{
     let sure = confirm('Вы уверены?');
     if (sure) {
         localStorage.clear();
-        taskDivGenerator();
+        taskContainer.innerHTML = '';
     }
 });
 
-taskDivGenerator();
+taskContainer.addEventListener('click', (el)=>{
+    if (el.target.tagName === 'LI') {
+        el.target.classList.toggle('checked');
+        saveData();
+    } else if (el.target.tagName === 'SPAN') {
+        el.target.parentElement.remove();
+        saveData();
+    }
+    
+});
+
+showData();
